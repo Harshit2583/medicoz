d4e5089d4fb4bac7ef6d3940227628b003ebff8f
Tanishk <tanishkraj70@gmail.com>
2024-09-17 15:17:37 +0530

    First ML added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
+        poppins: ["Poppins", "sans-serif"],
       },
       borderWidth: {
-        "30": "30px",
-<<<<<<< HEAD
-      },
-    },
-  },
-=======
+        30: "30px",
       },
     },
   },
->>>>>>> d98fe89b7c61cafc063bb9ce7173a719444d6601
   plugins: [],
-}
-
+};
