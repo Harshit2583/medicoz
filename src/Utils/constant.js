1243f96b8f319e8476645d880a1e989694bd0350
OmKeshri <omkeshri21@gmail.com>
2024-09-17 12:03:23 +0530

    added utils

---
+++ src/Utils/constant.js
@@ -0,0 +1,7 @@
+export const supported_languages = [
+  { identifier: "en", name: "English" },
+  { identifier: "हिन्दी", name: "हिन्दी" },
+  { identifier: "spanish", name: "Español" },
+  { identifier: "urdu", name: "اردو" },
+  { identifier: "french", name: "Français" },
+];
