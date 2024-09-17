2b1bf37d912b22cc85328f6593bce36947faefb7
Koushik <banerjeekoushik04@gmail.com>
2024-09-18 02:03:16 +0530

    commit

---
+++ src/Components/ShimmerPage/LoginSimmer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginSimmer = () => {
+  return (
+    <div className=''>
+        Login before coming to this page
+    </div>
+  )
+}
+
+export default LoginSimmer
