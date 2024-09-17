32f8347d6edc875b6f3182468a177db15a4c723a
Koushik <banerjeekoushik04@gmail.com>
2024-09-18 02:17:00 +0530

    commit

--- src/Components/ShimmerPage/LoginSimmer.js
+++ src/Components/ShimmerPage/LoginSimmer.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const LoginSimmer = () => {
   return (
-    <div className=''>
+    <div className='flex items-center justify-center h-screen w-screen bg-gray-100'>
+      <div className='text-center text-xl font-semibold text-gray-700'>
         Login before coming to this page
+      </div>
     </div>
-  )
+  );
 }
 
-export default LoginSimmer
+export default LoginSimmer;
