2c0c69c7dd6b25c2a2733734bc02ea05881b60f5
lavkush-kumar <lavkusharma993480@gmail.com>
2025-08-13 01:08:13 +0530

    Fix import name for LoginShimmer in Shopping.js and ensure consistent formatting

--- src/Components/ShoppingPage/Shopping.js
+++ src/Components/ShoppingPage/Shopping.js
@@ -1,20 +1,21 @@
 import { useSelector } from "react-redux";
 import Gradients from "../Common/Gradients";
 import MainContainer from "./MainContainer";
-import LoginSimmer from "../ShimmerPage/LoginShimmer";
+import LoginShimmer from "../ShimmerPage/LoginShimmer";
 
 const Shopping = () => {
-  const isLoggedIn = useSelector((store) => store.auth.isLoggedIn)
+  const isLoggedIn = useSelector((store) => store.auth.isLoggedIn);
 
-  if(isLoggedIn===false ) {
-    return <LoginSimmer />
+  if(isLoggedIn === false) {
+    return <LoginShimmer />;
   }
+  
   return (
     <div className="body-bg h-full pt-40 pb-20 overflow-hidden">
         <MainContainer />
         <Gradients />
     </div>
-  )
-}
+  );
+};
 
-export default Shopping
+export default Shopping;
